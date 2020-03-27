5a2e7d4aec9cda9a93d6a16d3868b7c303b84a0b
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2020-03-27 21:43:56 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
-$(function() {
-  let tabs = $(".tabs-item");
+// $(function() {
+//   let tabs = $(".tabs-item");
 
-  $(tabs).on("click", function(e) {
+//   $(tabs).on("click", function(e) {
+//     e.preventDefault();
+//     let activeContent = $(this).attr("href");
+
+//     $(".visible").toggleClass("visible");
+//     $(activeContent).toggleClass("visible");
+//     $(".tabs-item-active").toggleClass("tabs-item-active");
+//     $(this).toggleClass("tabs-item-active");
+//     console.log($(tabs));
+//   });
+// });
+
+const tabs = document.querySelectorAll(".tabs-item");
+const title = document.querySelector(".title");
+const head = document.querySelector(".header");
+const tabsContent = document.querySelectorAll(".tabs-content");
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function(e) {
     e.preventDefault();
-    let activeContent = $(this).attr("href");
-    // console.log($(".visible"));
-
-    $(".visible").toggleClass("visible");
-    $(activeContent).toggleClass("visible");
-    $(".tabs-item-active").toggleClass("tabs-item-active");
-    $(this).toggleClass("tabs-item-active");
-    console.log($(tabs));
+    const id = this.getAttribute("href");
+    const tabs = document.getElementById(id);
+ tabsContent.forEach(element => {
+      if (element.classList.contains("visible")) {
+        element.classList.remove("visible");
+      }
+    });
+    tabs.classList.add("visible");
+   
   });
-});
-let title = document.querySelector(".title");
-let head = document.querySelector(".header");
+}
+
 document.addEventListener("scroll", function(e) {
   if (window.pageYOffset > 50) {
     head.classList.add("position-fixed");
     title.classList.add("fadeInLeftBig");
- } else {
+  } else {
     head.classList.remove("position-fixed");
   }
 });
