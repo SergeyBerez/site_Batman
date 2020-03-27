a2662079b22d491a8e300729ae65b6115987a678
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2020-03-27 20:40:24 +0200

    add pos fixed

--- js/main.js
+++ js/main.js
@@ -4,10 +4,22 @@ $(function() {
   $(tabs).on("click", function(e) {
     e.preventDefault();
     let activeContent = $(this).attr("href");
-    console.log($(activeContent));
+    // console.log($(".visible"));
+
     $(".visible").toggleClass("visible");
     $(activeContent).toggleClass("visible");
     $(".tabs-item-active").toggleClass("tabs-item-active");
     $(this).toggleClass("tabs-item-active");
+    console.log($(tabs));
   });
+});
+let title = document.querySelector(".title");
+let head = document.querySelector(".header");
+document.addEventListener("scroll", function(e) {
+  if (window.pageYOffset > 50) {
+    head.classList.add("position-fixed");
+    title.classList.add("fadeInLeftBig");
+ } else {
+    head.classList.remove("position-fixed");
+  }
 });
