9d99f61d2a803b9ebb570cd817af1fc9e08b62b0
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2020-03-28 16:26:39 +0200

    1

--- js/main.js
+++ js/main.js
@@ -20,6 +20,9 @@ const tabsContent = document.querySelectorAll(".tabs-content");
 const title = document.querySelector(".title");
 const info = document.querySelector(".info");
 const tag = document.querySelector(".tag");
+const titleEn = document.querySelector(".title-en");
+const shortInfo = document.querySelector(".short-info");
+const fullInfo = document.querySelector(".full-info");
 for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", function(e) {
     e.preventDefault();
@@ -35,33 +38,57 @@ for (let i = 0; i < tabs.length; i++) {
 }
 
 document.addEventListener("scroll", function(e) {
-  console.log(1);
+  
   let top = Math.round(tag.getBoundingClientRect().top);
-  console.log(top);
+  
 
   if (window.pageYOffset > 1) {
     title.classList.add("fadeInLeftBig");
+    titleEn.classList.add("fadeInLeftBig");
     info.classList.add("fadeInRightBig");
 
-    if (document.querySelector(".fadeInLeftBig")) {
+    if (title.classList.contains("fadeInLeftBig")) {
       title.classList.remove("fadeOutLeftBig");
+      titleEn.classList.remove("fadeOutLeftBig");
       info.classList.remove("fadeOutRightBig");
     }
   } else if (
     window.pageYOffset < 1 &&
-    document.querySelector(".fadeInLeftBig")
+    title.classList.contains("fadeInLeftBig")
   ) {
-     title.classList.add("fadeOutLeftBig");
+    title.classList.add("fadeOutLeftBig");
+    titleEn.classList.add("fadeOutLeftBig");
     title.classList.remove("fadeInLeftBig");
+    titleEn.classList.remove("fadeInLeftBig");
     info.classList.add("fadeOutRightBig");
     info.classList.remove("fadeInRightBig");
   }
+  // -------------------
 
-  if (window.pageYOffset > 100) {
-    head.classList.add("position-fixed");
-  } else {
-    head.classList.remove("position-fixed");
+  if (window.pageYOffset > 150) {
+   console.log(1);
+    shortInfo.classList.add("fadeInLeftBig");
+    fullInfo.classList.add("fadeInRightBig");
+
+    if (shortInfo.classList.contains("fadeInLeftBig")) {
+      shortInfo.classList.remove("fadeOutLeftBig");
+      fullInfo.classList.remove("fadeOutRightBig");
+    }
+  } else if (
+    window.pageYOffset < 70 &&
+    shortInfo.classList.contains("fadeInLeftBig")
+  ) {
+    shortInfo.classList.add("fadeOutLeftBig");
+  fullInfo.classList.add("fadeOutRightBig");
+    shortInfo.classList.remove("fadeInLeftBig");
+    fullInfo.classList.remove("fadeInRightBig");
   }
+
+  // if (window.pageYOffset > 100) {
+  //   head.classList.add("position-fixed");
+  // } else {
+  //   head.classList.remove("position-fixed");
+  // }
   // if (window.pageYOffset > 70) {
   //   infoAnimated.classList.add("fadeInRightBig");
   // } else {
